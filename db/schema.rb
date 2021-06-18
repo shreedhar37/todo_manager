ea370f3356083b828f88af2ab6901ed7dadf113b
Shreedhar <meshree4@gmail.com>
2021-06-18 19:16:19 +0530

    Column name changed to 'password'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_133930) do
+ActiveRecord::Schema.define(version: 2021_06_18_133040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2021_06_17_133930) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "pswd"
+    t.string "password"
   end
 
 end
