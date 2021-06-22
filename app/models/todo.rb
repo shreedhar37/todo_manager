3fa481a70c8eabeed2fdd45f108dbb6969242392
Shreedhar <meshree4@gmail.com>
2021-06-22 14:27:09 +0530

    HTML CSS and ERB combined

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,39 @@
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
-    is_completed = completed ? "[x]" : "[ ]"
-    "#{id}. #{todo_text}  #{due_date.to_s(:long)}  #{is_completed}"
+  def due_today?
+    due_date == Date.today
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
+
+  def self.overdue
+    #Notice that self.overdue is a class method because of the self. prefix.
+    #Those methods are not available in individual objects of the class, but on the entire class itself.
+    all.where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.show_list
+    puts "\nMy Todo-List\n\nOverdue\n"
+    puts overdue.map { |todo| todo.to_displayable_string }
+    puts "\nDue Today\n"
+    puts due_today.map { |todo| todo.to_displayable_string }
+    puts "\nDue Later\n"
+    puts due_later.map { |todo| todo.to_displayable_string }
+  end
+
+  def self.mark_as_complete(id)
+    to_mark = Todo.find(id)  # To fetch the id of the given task from database.
+    to_mark.completed = true
+    to_mark.save # To commit the changes in the database.
+    to_mark
   end
 end
