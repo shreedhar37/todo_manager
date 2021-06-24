18a66116f481776588d77a22af272660261585f8
Shreedhar <meshree4@gmail.com>
2021-06-24 20:27:45 +0530

    Relation 'users' renamed to 'user'.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_24_142702) do
+ActiveRecord::Schema.define(version: 2021_06_24_145604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,9 +21,10 @@ ActiveRecord::Schema.define(version: 2021_06_24_142702) do
     t.boolean "completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "user", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "email"
