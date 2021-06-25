74c741d1254557224263778605cf9e7c7f052d74
Shreedhar <meshree4@gmail.com>
2021-06-25 13:31:34 +0530

    Use has_secure_password to save password_digest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
-      # password_digest: digest(params[:password_digest])
+      password: params[:password],
     )
     redirect_to "/"
   end
