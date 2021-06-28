b1546e471ef0153e872e65bbeef6b40a744679f0
Shreedhar <meshree4@gmail.com>
2021-06-28 19:38:09 +0530

    Duplicate email removed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   validates :first_name, :email, :password, presence: true
+  validates :email, uniqueness: true
   validates :password, length: { minimum: 8 }
   has_secure_password
   has_many :todos
